35e0b9d70634f086a1df53adb6c95ef3a2c14c6d
Vitali Vishneuski <vitali_vishneuski@epam.com>
2020-07-02 16:43:12 +0000

    Fix TestCase(new[] {-27, 987656789, 7557, int.MaxValue, 7556, 7243, 7243427, int.MinValue},             ExpectedResult = new[] {987656789, 7557, 7243427})

--- FilterByPalindromicTask.Tests/ArrayExtensionTests.cs
+++ FilterByPalindromicTask.Tests/ArrayExtensionTests.cs
@@ -11,7 +11,7 @@ namespace FilterByPalindromicTask.Tests
         [TestCase(new[] {2212332, 0, 1405644, 12345, 1, -1236674, 123321, 1111111}, ExpectedResult = new[] {0, 1, 123321, 1111111})]
         [TestCase(new[] {1111111112, 987654, -24, 1234654321, 32, 1005}, ExpectedResult = new int[] { })]
         [TestCase(new[] {-27, 987656789, 7557, int.MaxValue, 7556, 7243, 7243427, int.MinValue},
-            ExpectedResult = new[] {7557, 7243427})]
+            ExpectedResult = new[] {987656789, 7557, 7243427})]
         [TestCase(new[] {111, 111, 111, 11111111}, ExpectedResult = new[] {111, 111, 111, 11111111})]
         [TestCase(new[] {-1, 0, 111, -11, -1}, ExpectedResult = new [] {0, 111})]
         [TestCase(new[] {0, 1, 2, 3, 4}, ExpectedResult = new [] {0, 1, 2, 3, 4})]
